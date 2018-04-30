9d54d403c83519fadc58984d919f0850e6ecf507
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2018-04-30 16:11:23 -0300

    Set up the API

---
+++ src/main/java/org/tuiter/TuiterApiApplication.java
@@ -0,0 +1,12 @@
+package org.tuiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuiterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuiterApiApplication.class, args);
+	}
+}
