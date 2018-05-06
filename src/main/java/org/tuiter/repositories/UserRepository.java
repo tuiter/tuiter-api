d6e4b08bc9375d8041f8bedb8f1163026c4564a1
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-05-06 14:24:33 -0300

    Adding AuthController

--- src/main/java/org/tuiter/repositories/UserRepository.java
+++ src/main/java/org/tuiter/repositories/UserRepository.java
@@ -4,5 +4,9 @@ import org.tuiter.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
+	public User findByEmail(String email);
+
+	public User findByUsername(String username);
 	
 }
