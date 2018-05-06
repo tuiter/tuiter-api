0430b5d763595bf493506714d5958cf2b6b2dbb9
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-05-06 14:25:37 -0300

    Creating Constants class

---
+++ src/main/java/org/tuiter/util/ServerConstants.java
@@ -0,0 +1,9 @@
+package org.tuiter.util;
+
+public class ServerConstants {
+	public static final String AUTHENTICATION_REQUEST = "/authentication";
+	
+	public static final String TOKEN_KEY = "bichopiruleta";
+
+	public static final String USERS_REQUEST = "/users";
+}
