8ff5c89c4966152ba967d5903599276563d8f4a5
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-05-13 23:19:10 +0000

    Using email in login.

--- src/main/java/org/tuiter/beans/LoginBean.java
+++ src/main/java/org/tuiter/beans/LoginBean.java
@@ -3,16 +3,25 @@ package org.tuiter.beans;
 public class LoginBean {
 	private String email;
 	private String password;
+	private String username;
 	
 	public LoginBean(){
 		
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setUsername(String email) {
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
