1c739035b8632d7280aa3eee9758d589a59f4c55
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-05-20 15:53:59 -0300

    Adding controller

--- src/main/java/org/tuiter/controllers/UserController.java
+++ src/main/java/org/tuiter/controllers/UserController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,17 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.tuiter.beans.DeleteUserBean;
-import org.tuiter.beans.EditUserBean;
 import org.tuiter.beans.ResetPasswordBean;
 import org.tuiter.beans.SignupBean;
 import org.tuiter.errors.ErrorCode;
 import org.tuiter.errors.exceptions.IncorretPasswordException;
 import org.tuiter.errors.exceptions.TuiterApiException;
 import org.tuiter.errors.exceptions.UserAlreadyExistsException;
+import org.tuiter.errors.exceptions.UserNotExistsException;
 import org.tuiter.errors.exceptions.UserNotFoundException;
+import org.tuiter.models.Essay;
 import org.tuiter.models.User;
 import org.tuiter.services.implementations.UserServiceImpl;
+import org.tuiter.services.interfaces.EssayService;
 import org.tuiter.services.interfaces.UserService;
 import org.tuiter.util.ServerConstants;
 
@@ -32,6 +32,7 @@ import org.tuiter.util.ServerConstants;
 				+ ServerConstants.USER_REQUEST)
 public class UserController {
 	private UserService userService;
+	private EssayService essayService;
 	
 	@Autowired
 	public void setUserService(UserServiceImpl userService) {
@@ -96,6 +97,18 @@ public class UserController {
 		} catch (IncorretPasswordException e) {
 			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}		
+	}
+	
+	@RequestMapping(value = "/{id}/essays",
+			method = RequestMethod.GET
+			) 
+	public ResponseEntity<Iterable<Essay>> getEssaysByUser(@PathVariable String id) {
+		try {
+			return new ResponseEntity<Iterable<Essay>>(essayService.findAllByUserId(id), HttpStatus.OK);
+		} catch (UserNotExistsException e) {
+			throw new TuiterApiException("User not found!", HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.NOT_FOUND);			
+		}
+		
 	}
 }
 
