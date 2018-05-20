5914f1bf60bf56c550ad7226b391356ccf1a2fd8
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-05-20 15:53:42 -0300

    Adjusting tests

--- src/test/java/org/tuiter/models/EssayTest.java
+++ src/test/java/org/tuiter/models/EssayTest.java
@@ -3,30 +3,29 @@ package org.tuiter.models;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.tuiter.util.Gender;
 
 public class EssayTest {
-	User user1;
-	User user2;
+	String user1;
+	String user2;
 	Essay essay1;
 	Essay essay2;
 	Essay essay3;
 	
 	@Before
 	public void createUsersAndEssays () {
-		user1 = new User("joaoc@email.com", "joaoc", Gender.M, "João Clóvis", "123456");
-		user2 = new User("mariaa@email.com", "mariaa", Gender.F, "Maria Araújo", "543543543");
+		user1 = "1";
+		user2 = "2";
 		
-		essay1 = new Essay(user1, "Hoje e ontem", "Tempo", "Texto aqui");
-		essay2 = new Essay(user2, "Hoje e amanhã", "Tempo", "Outro texto aqui");
-		essay3 = new Essay(user1, "Pera e uva", "Comida", "Texto terceiro aqui");
+		essay1 = new Essay("1", "Hoje e ontem", "Tempo", "Texto aqui");
+		essay2 = new Essay("2", "Hoje e amanhã", "Tempo", "Outro texto aqui");
+		essay3 = new Essay("3", "Pera e uva", "Comida", "Texto terceiro aqui");
 	}
 	
 	@Test
 	public void userTest() {
-		Assert.assertEquals(essay1.getUser(), user1);
-		Assert.assertEquals(essay2.getUser(), user2);
-		Assert.assertEquals(essay3.getUser(), user1);
+		Assert.assertEquals(essay1.getUserId(), user1);
+		Assert.assertEquals(essay2.getUserId(), user2);
+		Assert.assertEquals(essay3.getUserId(), user1);
 		
 	}
 	
