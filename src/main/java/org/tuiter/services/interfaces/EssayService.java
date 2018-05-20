41997c7e6fd8a10b5907193d0e8d79dfa5801883
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-05-20 15:54:19 -0300

    Altering services

--- src/main/java/org/tuiter/services/interfaces/EssayService.java
+++ src/main/java/org/tuiter/services/interfaces/EssayService.java
@@ -1,17 +1,19 @@
 package org.tuiter.services.interfaces;
 
+import org.tuiter.beans.EditEssayBean;
 import org.tuiter.beans.modelbeans.EssayBean;
+import org.tuiter.errors.exceptions.EmptyFieldsException;
 import org.tuiter.errors.exceptions.EssayNotExistsException;
 import org.tuiter.errors.exceptions.UserNotExistsException;
 import org.tuiter.models.Essay;
 
 public interface EssayService {
 	public Essay create(EssayBean bean) throws UserNotExistsException;
-	public Essay update(EssayBean bean) throws UserNotExistsException, EssayNotExistsException;
+	Essay update(EditEssayBean bean) throws EssayNotExistsException, EmptyFieldsException;
 	public Essay delete(String id) throws EssayNotExistsException;
 	public Iterable<Essay> findAllByUserUsername(String username) throws UserNotExistsException;
 	public Iterable<Essay> findAllByUserId(String id) throws UserNotExistsException;
 	public Iterable<Essay> findAll();
 	public Essay findByTitleAndUserId(String title, String userId);
-	
+	public Essay findById(String id) throws EssayNotExistsException;	
 }
