5eff424f83be5d3f9596ff7c4d51f7b0953547ba
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-06-23 03:17:32 +0000

    Refactor review controller.

--- src/main/java/org/corrige/ai/controllers/ReviewController.java
+++ src/main/java/org/corrige/ai/controllers/ReviewController.java
@@ -2,9 +2,6 @@ package org.corrige.ai.controllers;
 
 import org.corrige.ai.models.review.EditReviewBean;
 import org.corrige.ai.models.review.Review;
-import org.corrige.ai.models.review.ReviewBean;
-import org.corrige.ai.services.implementations.NotificationServiceImpl;
-import org.corrige.ai.services.implementations.ReviewServiceImpl;
 import org.corrige.ai.services.interfaces.NotificationService;
 import org.corrige.ai.services.interfaces.ReviewService;
 import org.corrige.ai.util.ServerConstants;
@@ -16,10 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,27 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(ServerConstants.SERVER_REQUEST 
 				+ ServerConstants.REVIEW_REQUEST)
 public class ReviewController {
+	@Autowired
 	private NotificationService notificationService;
-	private ReviewService reviewService;	
-	
 	@Autowired
-	public void setReviewService(ReviewServiceImpl reviewService) {
-		this.reviewService = reviewService;
-	}
+	private ReviewService reviewService;
 	
-	@Autowired
-	public void setNotificatonService(NotificationServiceImpl notificationService) {
-		this.notificationService = notificationService;
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET) 
-	public ResponseEntity<Object> getById(@PathVariable String id) throws ReviewNotExistsException {
+	@GetMapping(value = "/{id}") 
+	public ResponseEntity<Review> getById(@PathVariable String id) throws ReviewNotExistsException {
 		Review review = reviewService.findById(id);
 		return new ResponseEntity<>(review, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/{id}", method = RequestMethod.PUT) 
-	public ResponseEntity<Object> update(@PathVariable String id, @RequestBody EditReviewBean body) throws ReviewNotExistsException, EmptyFieldsException, EssayNotExistsException, UserNotExistsException {
+	@PutMapping(value="/{id}") 
+	public ResponseEntity<Review> update(@PathVariable String id, @RequestBody EditReviewBean body) throws ReviewNotExistsException, EmptyFieldsException, EssayNotExistsException, UserNotExistsException {
 		Review review = reviewService.update(id, body);
 		notificationService.createOnReviewDone(review.getEssayId(), review.getUserId());
 		return new ResponseEntity<>(review, HttpStatus.OK);
